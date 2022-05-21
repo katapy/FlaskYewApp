9cb174aa93ab02b260d824eb30d4806d0d8cfd98
katapy <katapy.tennis@gmail.com>
2022-05-21 16:41:12 +0900

    Add ws server

--- server/app/hello.py
+++ server/app/hello.py
@@ -1,9 +1,11 @@
 
+# -*- coding: utf-8 -*-
 import os
-from flask import Flask
-from flask import render_template
- 
+from flask import Flask, render_template
+from flask_sock import Sock
+
 app = Flask(__name__)
+sock = Sock(app)
 
 @app.route("/")
 def hello():
@@ -13,6 +15,12 @@ def hello():
 def page_not_found(error):
     """If 404 error, go to yew 404 error page."""
     return render_template('index.html')
+
+@sock.route('/ws')
+def echo(ws):
+    while True:
+        data = ws.receive()
+        ws.send("ws server send: " + data)
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
